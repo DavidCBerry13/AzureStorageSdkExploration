862c812a1306d628d7b3a44d6cdd3307c6a4e5ce
David Berry <dberry83702@yahoo.com>
2022-11-23 12:38:16 -0600

    Code cleanup - move things into parameters to make the SDK code clearer in what code a user would have to write

--- AzureBlobProposal/Program.cs
+++ AzureBlobProposal/Program.cs
@@ -24,28 +24,35 @@ class Program
             return;
         }
 
-        CleanStart(storageConnectionString);
+        CleanStart(storageConnectionString, logosContainer, devLanguagesContainer, devLanguagescontainer2);
 
-        UploadFileToStorageCurrent(storageConnectionString);
-        UploadFileToStorageProposed(storageConnectionString);
+        UploadFileToStorageCurrent(storageConnectionString, logosContainer, "azure-logo.svg", "images/azure.svg");
+        UploadFileToStorageProposed(storageConnectionString, logosContainer, "microsoft-logo.svg", "images/microsoft-logo.svg");
 
-        UploadMultipleCurrent(storageConnectionString);
-        UploadMultipleProposed(storageConnectionString);
+        UploadMultipleCurrent(storageConnectionString, devLanguagesContainer, devLanguagesImages);
+        UploadMultipleProposed(storageConnectionString, devLanguagescontainer2, devLanguagesImages);
 
-        UploadFileToStorageWithContentTypeCurrent(storageConnectionString);
-        UploadFileToStorageWithContentTypeProposed(storageConnectionString);
+        UploadFileToStorageWithContentTypeCurrent(storageConnectionString, logosContainer, "azure-logo-with-content-type.svg", "images/azure.svg");
+        UploadFileToStorageWithContentTypeProposed(storageConnectionString, logosContainer, "microsoft-logo-with-content-type.svg", "images/microsoft-logo.svg");
 
-        UploadFileToStorageBestSolution(storageConnectionString);
+        UploadFileToStorageBestSolution(storageConnectionString, devLanguagesContainer, "dotnet-core.svg", "images/dotnet-core.svg");
+
+        Console.WriteLine("\nDev Language Blobs");
+        Console.WriteLine("----------------------------------------------------------------------");
+        GetBlobs(storageConnectionString, "dev-languages");
     }
 
+    private static string logosContainer = "logos";
+    private static string devLanguagesContainer = "dev-languages";
+    private static string devLanguagescontainer2 = "dev-languages-new";
+    private static string[] devLanguagesImages = new[] { "images/csharp.svg", "images/golang.svg", "images/java.svg", "images/javascript.svg", "images/python.svg" };
 
 
-    public static void UploadFileToStorageCurrent(string storageConnectionString)
+
+
+
+    public static void UploadFileToStorageCurrent(string storageConnectionString, string containerName, string blobName, string filePath)
     {
-        string containerName = "logos";
-        string blobName = "azure-logo.svg";
-        string filePath = "images/azure.svg";
-
         BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
 
         BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
@@ -57,29 +64,16 @@ class Program
 
 
 
-    public static void UploadFileToStorageProposed(string storageConnectionString)
+    public static void UploadFileToStorageProposed(string storageConnectionString, string containerName, string blobName, string filePath)
     {
-        string containerName = "logos";
-        string blobName = "microsoft-logo.svg";
-        string filePath = "images/microsoft-logo.svg";
-
         BlobStorageClient blobStorageClient = new BlobStorageClient(storageConnectionString);
         blobStorageClient.CreateBlobContainerIfNotExists(containerName);
         blobStorageClient.Upload(containerName, blobName, filePath);
     }
 
 
-    public static void UploadMultipleCurrent(string storageConnectionString)
+    public static void UploadMultipleCurrent(string storageConnectionString, string containerName, string[] files)
     {
-        string containerName = "dev-languages";
-        var csharp = (BlobName: "csharp.svg", FilePath: "images/csharp.svg");
-        var golang = (BlobName: "golang.svg", FilePath: "images/golang.svg");
-        var java = (BlobName: "java.svg", FilePath: "images/java.svg");
-        var javascript = (BlobName: "javascript.svg", FilePath: "images/javascript.svg");
-        var python = (BlobName: "python.svg", FilePath: "images/python.svg");
-        var files = new[] { csharp, golang, java, javascript, python };
-
-        // Here starts the code
         BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
 
         BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
@@ -87,62 +81,55 @@ class Program
 
         foreach (var file in files)
         {
-            BlobClient blobClient = blobContainerClient.GetBlobClient(file.BlobName);
-            var response = blobClient.Upload(file.FilePath);
+            var blobName = Path.GetFileName(file);
+            var filePath = file;
+
+            BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+            var response = blobClient.Upload(file);
         }        
     }
 
-    public static void UploadMultipleProposed(string storageConnectionString)
+    public static void UploadMultipleProposed(string storageConnectionString, string containerName, string[] files)
     {
-        string containerName = "dev-languages-new";
-        var csharp = (BlobName: "csharp.svg", FilePath: "images/csharp.svg");
-        var golang = (BlobName: "golang.svg", FilePath: "images/golang.svg");
-        var java = (BlobName: "java.svg", FilePath: "images/java.svg");
-        var javascript = (BlobName: "javascript.svg", FilePath: "images/javascript.svg");
-        var python = (BlobName: "python.svg", FilePath: "images/python.svg");
-        var files = new[] { csharp, golang, java, javascript, python };
-
         // Here starts the code
         BlobStorageClient blobStorageClient = new BlobStorageClient(storageConnectionString);
 
         blobStorageClient.CreateBlobContainerIfNotExists(containerName);
 
         foreach (var file in files)
-            blobStorageClient.Upload(containerName, file.BlobName, file.FilePath);
-        
-        // Or even better
-        // files.Select(f => blobStorageClient.Upload(containerName, f.BlobName, f.FilePath));
+        {
+            var blobName = Path.GetFileName(file);
+            var filePath = file;
 
+            blobStorageClient.Upload(containerName, blobName, filePath);
+        }
+
+        //foreach (var file in files)
+        //    blobStorageClient.Upload(containerName, Path.GetFileName(file), file);
+
+        // Or even better
+        // files.Select(file => blobStorageClient.Upload(containerName, Path.GetFileName(file), file));
     }
 
 
 
 
 
-    public static void UploadFileToStorageWithContentTypeCurrent(string storageConnectionString)
+    public static void UploadFileToStorageWithContentTypeCurrent(string storageConnectionString, string containerName, string blobName, string filePath)
     {
-        string containerName = "logos";
-        string blobName = "azure-logo-with-content-type.svg";
-        string filePath = "images/azure.svg";
-
         BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
 
         BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
         blobContainerClient.CreateIfNotExists();
 
         BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
-
         var options = new BlobUploadOptions() { HttpHeaders = new BlobHttpHeaders() { ContentType = "image/svg+xml" } };        
         var response = blobClient.Upload(filePath, options);
     }
 
 
-    public static void UploadFileToStorageWithContentTypeProposed(string storageConnectionString)
+    public static void UploadFileToStorageWithContentTypeProposed(string storageConnectionString, string containerName, string blobName, string filePath)
     {
-        string containerName = "logos";
-        string blobName = "microsoft-logo-with-content-type.svg";
-        string filePath = "images/microsoft-logo.svg";
-
         BlobStorageClient blobStorageClient = new BlobStorageClient(storageConnectionString);
         blobStorageClient.CreateBlobContainerIfNotExists(containerName);
 
@@ -150,62 +137,50 @@ class Program
     }
 
 
-    public static void UploadFileToStorageBestSolution(string storageConnectionString)
+    public static void UploadFileToStorageBestSolution(string storageConnectionString, string containerName, string blobName, string filePath)
     {
-        string containerName = "dev-languages";
-        string blobName = "dotnet-core.svg";
-        string filePath = "images/dotnet-core.svg";
-
         BlobStorageClient blobStorageClient = new BlobStorageClient(storageConnectionString, autoCreateContainers: true);
         blobStorageClient.Upload(containerName, blobName, filePath, "image/svg+xml");
     }
 
 
-
-    public static void CleanStart(string storageConnectionString)
+    public static void GetBlobs(string storageConnectionString, string containerName)
     {
-        string logosContainerName = "logos";
-        string[] logoBlobNames = new string[] { "azure-logo.svg", "microsoft-logo.svg", "microsoft-logo-with-content-type.svg", "dotnet-core.svg"};
+        BlobServiceClient blobServiceClient = new BlobServiceClient(storageConnectionString);
 
-        BlobStorageClient blobStorageClient = new BlobStorageClient(storageConnectionString);
-        if ( blobStorageClient.ContainerExists(logosContainerName))
+        BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+        var blobs = blobContainerClient.GetBlobs();
+
+        foreach (var blob in blobs)
         {
-            foreach (var blobName in logoBlobNames)
-            {
-                blobStorageClient.DeleteBlobIfExists(logosContainerName, blobName);
-            }
-            //blobStorageClient.DeleteBlobContainer(logosContainerName);
-        }
-
-        string languagesContainerName = "dev-languages";
-        var csharp = (BlobName: "csharp.svg", FilePath: "images/csharp.svg");
-        var golang = (BlobName: "golang.svg", FilePath: "images/golang.svg");
-        var java = (BlobName: "java.svg", FilePath: "images/java.svg");
-        var javascript = (BlobName: "javascript.svg", FilePath: "images/javascript.svg");
-        var python = (BlobName: "python.svg", FilePath: "images/python.svg");
-        var dotnet = (BlobName: "dotnet-core.svg", FilePath: "images/dotnet-core.svg");
-        var languageBlobs = new[] { csharp, golang, java, javascript, python, dotnet };
-
-        if (blobStorageClient.ContainerExists(languagesContainerName))
-        {
-            foreach (var lang in languageBlobs)
-            {
-                blobStorageClient.DeleteBlobIfExists(languagesContainerName, lang.BlobName);
-            }
-            //blobStorageClient.DeleteBlobContainer(logosContainerName);
-        }
-
-
-        string loopContainerName = "dev-languages-new";
-        if (blobStorageClient.ContainerExists(loopContainerName))
-        {
-            foreach (var lang in languageBlobs)
-            {
-                blobStorageClient.DeleteBlobIfExists(loopContainerName, lang.BlobName);
-            }
-            //blobStorageClient.DeleteBlobContainer(logosContainerName);
+            Console.WriteLine($"{blob.Name.PadRight(20)}    {blob.Properties.CreatedOn}   {blob.Properties.ContentLength}  {blob.Properties.AccessTier?.ToString()}");
         }
     }
+
+
+
+    public static void CleanStart(string storageConnectionString, params string[] containers)
+    {
+        BlobStorageClient blobStorageClient = new BlobStorageClient(storageConnectionString);
+        foreach (var container in containers)
+        {
+            if (blobStorageClient.ContainerExists(container))
+            {
+                var blobs = blobStorageClient.GetBlobs(container);
+
+                foreach (var blob in blobs)
+                {
+                    blobStorageClient.DeleteBlobIfExists(container, blob.Name);
+                }
+                //blobStorageClient.DeleteBlobContainer(logosContainerName);
+            }
+        }
+    }
+
+
+
+
+
 
 }
 
